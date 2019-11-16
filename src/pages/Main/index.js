25637ad8a98d9e41c9d8165f897ea1a60fbb714d
iltonkp <iltonk.si@gmail.com>
2019-11-16 18:49:26 -0300

    create function handleAddUser from search users on github api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,25 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Keyboard} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import Api from '~/services/api';
 import {Container, Form, Input, SubmitButton} from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar Usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#FFF" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
-}
+export default class Main extends Component {
+  static navigationOptions = {
+    headerTitle: 'Usuários',
+  };
 
-Main.navigationOptions = {
-  headerTitle: 'Usuários',
-};
+  state = {
+    users: [],
+    newUser: '',
+  };
+
+  handleAddUser = async () => {
+    const {users, newUser} = this.state;
+
+    const response = await Api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const {users, newUser} = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar Usuário"
+            value={newUser}
+            onChangeText={text => this.setState({newUser: text})}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}

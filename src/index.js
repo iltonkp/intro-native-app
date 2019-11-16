c76ebb170e49e7863c5312dfd754cde9c6dd2ee0
iltonkp <iltonk.si@gmail.com>
2019-11-16 16:54:51 -0300

    configure statusBar and react-navigation-stack

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import '~/config/ReactotronConfig';
 
 import Routes from './routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar backgroundColor="#7159c1" barStyle="light-content" />
+      <Routes />
+    </>
+  );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,27 @@
-import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 import Main from '~/pages/Main';
+import User from '~/pages/User';
 
-const Routes = createAppContainer(createSwitchNavigator({Main}));
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#FFF',
+      },
+    },
+  ),
+);
 
 export default Routes;

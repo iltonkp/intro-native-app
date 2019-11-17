1b431f5c4b9c0d2525e75b5e2ce46632f31f2024
iltonkp <iltonk.si@gmail.com>
2019-11-16 21:30:37 -0300

    configure activityIdicator

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Keyboard} from 'react-native';
+import {Keyboard, ActivityIndicator} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Api from '~/services/api';
@@ -25,10 +25,13 @@ export default class Main extends Component {
   state = {
     users: [],
     newUser: '',
+    loading: false,
   };
 
   handleAddUser = async () => {
     const {users, newUser} = this.state;
+
+    this.setState({loading: true});
 
     const response = await Api.get(`/users/${newUser}`);
 
@@ -42,13 +45,14 @@ export default class Main extends Component {
     this.setState({
       users: [...users, data],
       newUser: '',
+      loading: false,
     });
 
     Keyboard.dismiss();
   };
 
   render() {
-    const {users, newUser} = this.state;
+    const {users, newUser, loading} = this.state;
 
     return (
       <Container>
@@ -62,8 +66,12 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} color="#FFF" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={20} color="#FFF" />
+            )}
           </SubmitButton>
         </Form>
 
